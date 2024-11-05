02928e2d6802d16ffc946513f98241d4609dd13c
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-11-05 15:00:15 +0100

    feat: add function for getting array of rnd elements from array

--- src/helpers/getRndElement.js
+++ src/helpers/getRndElement.js
@@ -7,3 +7,15 @@ export function getRndElement(array) {
     index,
   };
 }
+
+export function getRndNElements(count, array) {
+  const copy = [...array];
+  const result = [];
+
+  while (count > result.length) {
+    const { index } = getRndElement(copy);
+    copy.splice(index, 1);
+  }
+
+  return result;
+}
