41d3aa10d437dfdfeb8cb5a0cc9e33ec45a9a951
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-11-08 11:21:59 +0100

    feat: add handleCardClick for game play win and loss

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,8 +1,66 @@
-import { mdiHome, mdiReload, mdiCrown, mdiClock } from '@mdi/js';
+import { useEffect, useState } from 'react';
+import {
+  mdiHome,
+  mdiReload,
+  mdiCrown,
+  mdiClock,
+  mdiStar,
+  mdiClockOutline,
+} from '@mdi/js';
 import Icon from '@mdi/react';
 import '../styles/GamePlay.css';
+import placeHolderData from '../assets/data.js';
+import pickRandom from 'pick-random';
 
 function GamePlay({ onClose }) {
+  const placeHolderDataCode = placeHolderData.map((data) => {
+    const { code } = data;
+
+    return code;
+  });
+  const [viewed, setViewed] = useState([]);
+  const [notViewed, setNotViewed] = useState(placeHolderDataCode);
+  const [playCards, setPlayCards] = useState(
+    pickRandom(placeHolderDataCode, { count: 4 })
+  );
+  const [gameEnded, setGameEnded] = useState(false);
+
+  function handleCardClick(code) {
+    if (gameEnded) {
+      console.log('game won');
+      return;
+    } // game won
+
+    if (viewed.includes(code)) {
+      console.log(' game lost ');
+      return;
+    } // game lost
+
+    const updatedUniqueElements = notViewed.filter((d) => d !== code);
+
+    if (updatedUniqueElements.length == 0) {
+      setGameEnded(true);
+      return;
+    }
+
+    const [uniqueElement] = pickRandom(updatedUniqueElements);
+    const updatedViewed = [code, ...viewed];
+
+    const temp = updatedUniqueElements.filter((d) => d !== uniqueElement);
+
+    const temp2 = pickRandom([...temp, ...updatedViewed], {
+      count: 3,
+    });
+
+    const updatedPlayCards = pickRandom([uniqueElement, ...temp2], {
+      count: 4,
+    });
+
+    setPlayCards(updatedPlayCards);
+    setNotViewed(updatedUniqueElements);
+    setViewed(updatedViewed);
+  }
+
   function handlePageClose() {
     onClose();
   }
@@ -48,10 +106,16 @@ function GamePlay({ onClose }) {
       <div className='game-area padding_2r d-flex__col gap_2r'>
         <header className='d-flex__col'>
           <div className='d-flex__row align-items__center justify-content__space-between padding-left_1r padding-right_1r'>
-            <div className='score-container'>
+            <div className='score-container d-flex__row align-items__center'>
+              <span className='icon-container'>
+                <Icon path={mdiStar} />
+              </span>
               <span>728</span>
             </div>
-            <div className='timer-container'>
+            <div className='timer-container d-flex__row align-items__center'>
+              <span className='icon-container'>
+                <Icon path={mdiClockOutline} />
+              </span>
               <span>00:49</span>
             </div>
           </div>
@@ -61,7 +125,27 @@ function GamePlay({ onClose }) {
             </div>
           </div>
         </header>
-        <div className='game-cards cards-container'></div>
+        <div className='game-cards'>
+          {placeHolderData
+            .filter((data) => playCards.includes(data.code))
+            .map((data) => {
+              const { image, name, code } = data;
+              return (
+                <div
+                  className='card d-flex__row align-items__center justify-content__center cursor__pointer padding_2r gap_1r'
+                  key={code}
+                  onClick={() => {
+                    handleCardClick(code);
+                  }}
+                >
+                  <div className='card-image'>
+                    <img src={image} alt={name} />
+                  </div>
+                  <div className='card-text'>{code}</div>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
