83b83c64e7e89369ffeff12894473865776858c4
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-11-23 03:34:47 +0100

    feat: fix lint errors

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -63,41 +63,41 @@ function HomePage({ emojis }) {
   }
 
   return (
-    <div className='home-page d-flex__col gap_2r align-items__center justify-content__center'>
+    <div className="home-page d-flex__col gap_2r align-items__center justify-content__center">
       <div>
-        <div className='icon-container icon-container__large'>
+        <div className="icon-container icon-container__large">
           {/* <img src={BrainUrl} alt='Brain' className='img' /> */}
         </div>
-        <h1 className='text-transform__uppercase'>Memory Game</h1>
+        <h1 className="text-transform__uppercase">Memory Game</h1>
       </div>
-      <div className='home-page-buttons d-flex__col align-items__center justify-content__center padding_2r'>
-        <ul className='d-flex__col gap_1r '>
-          <li className='d-flex__col align-items__center justify-content__space-around'>
+      <div className="home-page-buttons d-flex__col align-items__center justify-content__center padding_2r">
+        <ul className="d-flex__col gap_1r ">
+          <li className="d-flex__col align-items__center justify-content__space-around">
             <button
-              type='button'
-              className='btn home-page-btn d-flex__row gap_1r align-items__center'
+              type="button"
+              className="btn home-page-btn d-flex__row gap_1r align-items__center"
               onClick={openGamePlayPage}
             >
               <Icon path={mdiPlay} size={2} />
-              <span className='text-transform__capitalize'>play</span>
+              <span className="text-transform__capitalize">play</span>
             </button>
           </li>
-          <li className='d-flex__col align-items__center justify-content__space-around'>
+          <li className="d-flex__col align-items__center justify-content__space-around">
             <button
-              type='button'
-              className='btn home-page-btn d-flex__row gap_1r align-items__center'
+              type="button"
+              className="btn home-page-btn d-flex__row gap_1r align-items__center"
             >
               <Icon path={mdiHelp} size={2} />
-              <span className='text-transform__capitalize'>how to play</span>
+              <span className="text-transform__capitalize">how to play</span>
             </button>
           </li>
-          <li className='d-flex__col align-items__center justify-content__space-around'>
+          <li className="d-flex__col align-items__center justify-content__space-around">
             <button
-              type='button'
-              className='btn home-page-btn d-flex__row gap_1r align-items__center'
+              type="button"
+              className="btn home-page-btn d-flex__row gap_1r align-items__center"
             >
               <Icon path={mdiBookAccount} size={2} />
-              <span className='text-transform__capitalize'>scores</span>
+              <span className="text-transform__capitalize">scores</span>
             </button>
           </li>
         </ul>
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,11 +3,11 @@ import '../styles/Loader.css';
 
 function Loader({ loading }) {
   return (
-    <div className='loader-container d-flex__col align-items__center justify-content__center gap_2r'>
-      <h1 className='padding_2r text-transform__uppercase loading-header'>
+    <div className="loader-container d-flex__col align-items__center justify-content__center gap_2r">
+      <h1 className="padding_2r text-transform__uppercase loading-header">
         emoji flippers
       </h1>
-      <PropagateLoader color='#ffffff' loading={loading} size={15} />
+      <PropagateLoader color="#ffffff" loading={loading} size={15} />
     </div>
   );
 }
