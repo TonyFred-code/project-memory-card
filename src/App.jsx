c475151347488f7477791655f64c12a72b532ddb
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-11-23 05:16:34 +0100

    feat: rearrange import order

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,58 @@
 import { useState, useEffect } from 'react';
-import HomePage from './components/HomePage';
-import Loader from './components/Loader.jsx';
-import './styles/Base.css';
 import pickRandom from 'pick-random';
 import randomInteger from 'random-int';
+import HomePage from './components/HomePage';
+import Loader from './components/Loader';
+import './styles/Base.css';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [emojis, setEmojis] = useState([]);
   const [error, setError] = useState(null);
 
-  const groups = [
-    'people_body',
-    'component',
-    'smileys_emotion',
-    'animals_nature',
-    'food_drink',
-    'travel_places',
-    'activities',
-    'objects',
-    'symbols',
-    'flags',
-  ];
-
-  const CACHE_KEY = 'emoji_cache';
-  const CACHE_TIME_KEY = 'emoji_cache_time';
-  const CACHE_EXPIRATION = 60 * 60 * 1000; // 1 hour in milliseconds
-  const LOADER_DURATION = randomInteger(1099, 1900);
-
   useEffect(() => {
+    const groups = [
+      'people_body',
+      'component',
+      'smileys_emotion',
+      'animals_nature',
+      'food_drink',
+      'travel_places',
+      'activities',
+      'objects',
+      'symbols',
+      'flags',
+    ];
+
+    const CACHE_KEY = 'emoji_cache';
+    const CACHE_TIME_KEY = 'emoji_cache_time';
+    const CACHE_EXPIRATION = 60 * 60 * 1000; // 1 hour in milliseconds
+    const LOADER_DURATION = randomInteger(1099, 1900);
+
     async function fetchEmojis() {
-      try {
-        const baseUrl = 'https://api.api-ninjas.com/v1/emoji?';
-        const [group] = pickRandom(groups);
-        const offset = 10;
+      const baseUrl = 'https://api.api-ninjas.com/v1/emoji?';
+      const [group] = pickRandom(groups);
+      const offset = 10;
 
-        const url = `${baseUrl}group=${group}&offset=${offset}`;
-        const response = await fetch(url, {
-          method: 'GET',
-          headers: {
-            'X-API-Key': 'e+54iau6M600oDTT3LBh3w==bVb76eh0D03vSEEz',
-          },
-        });
+      const url = `${baseUrl}group=${group}&offset=${offset}`;
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'X-API-Key': 'e+54iau6M600oDTT3LBh3w==bVb76eh0D03vSEEz',
+        },
+      });
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        // Store data and timestamp in localStorage
-        localStorage.setItem(CACHE_KEY, JSON.stringify(data));
-        localStorage.setItem(CACHE_TIME_KEY, Date.now().toString());
-
-        return data;
-      } catch (err) {
-        throw err;
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
+
+      const data = await response.json();
+
+      // Store data and timestamp in localStorage
+      localStorage.setItem(CACHE_KEY, JSON.stringify(data));
+      localStorage.setItem(CACHE_TIME_KEY, Date.now().toString());
+
+      return data;
     }
 
     function loadCachedEmojis() {
