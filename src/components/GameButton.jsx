f6ac0db11a18b9c7fd992f09b2adb01ef8f39416
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-12-12 10:54:53 +0100

    feat: add disabled state parameter and logic

--- src/components/GameButton.jsx
+++ src/components/GameButton.jsx
@@ -1,21 +1,28 @@
 import useSound from 'use-sound';
 import selectSfx from '../assets/select-sound.mp3';
 
-function GameButton({ classNames, sfx, content, func }) {
+function GameButton({ classNames, sfx, content, func, disabled = false }) {
   const [playBtnClick] = useSound(selectSfx);
+
+  function handleBtnClick(event) {
+    if (event.target.disabled) return;
+
+    if (sfx) {
+      playBtnClick();
+    }
+
+    if (func) {
+      func();
+    }
+  }
 
   return (
     <button
       type="button"
+      disabled={disabled}
       className={classNames}
-      onClick={() => {
-        if (sfx) {
-          playBtnClick();
-        }
-
-        if (func) {
-          func();
-        }
+      onClick={(e) => {
+        handleBtnClick(e);
       }}
     >
       {content}
